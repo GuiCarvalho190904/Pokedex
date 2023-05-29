b41723254479a5e8da3a283c8f3a929e7518c4d8
GuiCarvalho190904 <gui.carvalho10city@gmail.com>
2023-05-29 17:07:09 -0300

    Kanto

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const botaoAlterarTema = document.getElementById
+    ("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroAtivo) {
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sol.png");
+
+    } else {
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/lua.png");
+    }
+});
